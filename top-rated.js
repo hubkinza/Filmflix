2e2f3596823255af63edc40c4e564b9d2e901472
kinzaqureshi <kinza.qureshi@persivia.com>
2025-07-19 16:02:31 +0500

    Api and base url to top -rated , add main to top-rated  html

---
+++ top-rated.js
@@ -0,0 +1,5 @@
+const apiKey = "ac575a88671f952e2c572e2556c26cda"; // API key
+const baseURL = "https://api.themoviedb.org/3"; // API base URL
+
+const movieContainer = document.getElementById("movieContainer"); // movie section
+const errorMessage = document.getElementById("errorMessage"); // error box
